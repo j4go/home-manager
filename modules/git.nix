cb3cec7910241a33c6522bfb6089329c18395b49
j4go <yianny@163.com>
2026-01-27 10:42:51 +0800

    Update from rocky: 2026-01-27 10:42:51

--- modules/git.nix
+++ modules/git.nix
@@ -1,14 +1,21 @@
 { pkgs, vars, ... }: {
   programs.git = {
     enable = true;
-    userName = "j4go";
-    userEmail = "yianny@163.com";
-    
-    extraConfig = {
-      push.autoSetupRemote = true;
-      # 🚀 动态注入代理
-      http.proxy = "${vars.proxyUrl}";
-      https.proxy = "${vars.proxyUrl}";
+    settings = {
+      user = {
+        name = "j4go";
+        email = "yianny@163.com";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      # 动态注入代理
+      http = {
+        proxy = "${vars.proxyUrl}";
+      };
+      https = {
+        proxy = "${vars.proxyUrl}";
+      };
     };
   };
 }
