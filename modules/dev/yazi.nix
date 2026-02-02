3b2da54bd9af75383a22d2d1f49705e95e7a36ed
j4go <yianny@163.com>
2026-02-03 07:38:27 +0800

    Update: 2026-02-03 07:38:16

--- modules/dev/yazi.nix
+++ modules/dev/yazi.nix
@@ -3,7 +3,7 @@
     enable = true;
     # 这会自动在 .bashrc 中注入一个 `y` 函数
     # 使用 `y` 命令启动 yazi，退出时会自动 cd 到当前目录
-    enableBashIntegration = true;
+    enableBashIntegration = false;
     # 可以在这里配置 yazi 的具体行为
     settings = {
       manager = {
--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -186,6 +186,17 @@ in {
           export all_proxy="http://${proxy.address}"
           echo "🌐 Proxy enabled: ${proxy.address}"
         ''}
+
+        # yazi y function
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+              builtin cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+        }
+
       '';
     };
   };
