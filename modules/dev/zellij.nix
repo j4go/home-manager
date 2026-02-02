33281a2bb438a53313f61476e25f97cf8c825726
j4go <yianny@163.com>
2026-02-02 17:53:21 +0800

    Update: 2026-02-02 17:52:40

--- modules/dev/zellij.nix
+++ modules/dev/zellij.nix
@@ -1,33 +1,29 @@
 {pkgs, ...}: {
   programs.zellij = {
     enable = true;
-    # 自动集成到 Bash 设置为true的话每次会自动进入zellij,打开一个新的会话
-    # 这里要设置为false
     enableBashIntegration = false;
 
-    # 核心设置
     settings = {
-      # 选择主题
-      theme = "everforest-light";
-      # 默认布局
+      # 1. 更改选用的主题名称
+      theme = "everforest-dark";
+
       default_layout = "compact";
-      # 鼠标支持
       mouse_mode = true;
-      # 简写模式：不显示底部提示栏 (熟练后开启)
       pane_frames = false;
 
-      everforest-light = {
-        fg = "#5c6a72";
-        bg = "#f3f5d9";
-        black = "#282e33";
-        red = "#f85552";
-        green = "#8da101";
-        yellow = "#dfa000";
-        blue = "#3a94c5";
-        magenta = "#df69ba";
-        cyan = "#35a77c";
-        white = "#dfddc8";
-        orange = "#f57d26";
+      # 2. 定义深色主题色值 (Medium 对比度)
+      everforest-dark = {
+        fg = "#d3c6aa"; # 浅灰色文字
+        bg = "#2d353b"; # 深绿色背景
+        black = "#475258"; # 亮黑（灰色）
+        red = "#e67e80";
+        green = "#a7c080";
+        yellow = "#dbbc7f";
+        blue = "#7fbbb3";
+        magenta = "#d699b6";
+        cyan = "#83c092";
+        white = "#d3c6aa";
+        orange = "#e69875";
       };
     };
   };
