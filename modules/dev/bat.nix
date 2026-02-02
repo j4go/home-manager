7b9d914e8893022b07ce3ab8a66a2172a193ed54
j4go <yianny@163.com>
2026-02-03 04:42:43 +0800

    Update: 2026-02-03 04:42:33

--- modules/dev/bat.nix
+++ modules/dev/bat.nix
@@ -5,16 +5,17 @@
 }: {
   programs.bat = {
     enable = true;
+
     # 1. 基础配置 (主题与行为)
     config = {
       pager = "less -FR"; # 保持类似 less 的翻页体验
-      theme = "gruvbox-light"; # 设置你喜欢的主题
-      # style = "numbers,changes"; # 可选：简约风格
+      theme = "Dracula"; # 设置你喜欢的主题
+      # style = "numbers,changes";  # 可选：简约风格
       style = "header,grid,numbers";
     };
   };
 
-  # 2. 额外插件 (强烈建议保留，太好用了)
+  # 2. 额外插件
   home.packages = with pkgs.bat-extras; [
     batman # 让 man 命令也带高亮
     batgrep # 配合 ripgrep 显示高亮
