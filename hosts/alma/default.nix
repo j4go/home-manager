d8b8006f8c16c2907fd85f496c10909835507aa1
j4go <yianny@163.com>
2026-02-03 17:07:06 +0800

    Update: add new host alma

---
+++ hosts/alma/default.nix
@@ -0,0 +1,18 @@
+{...}: {
+  imports = [
+    ../../modules/common-import.nix
+  ];
+
+  # 🚀 为这台机器量身定做开关
+  myOptions = {
+    proxy = {
+      enable = true;
+      address = "10.255.126.1:10808";
+    };
+    shell.starship.enable = true;
+  };
+
+  # 机器特定的基本信息
+  home.username = "w";
+  home.homeDirectory = "/home/w";
+}
