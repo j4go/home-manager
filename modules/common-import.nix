3273cc6415bb237a698985effd37bd9877d1e0d1
j4go <yianny@163.com>
2026-02-11 15:59:48 +0800

    Update: 整理module目录、

--- modules/common-import.nix
+++ modules/common-import.nix
@@ -1,8 +1,9 @@
 {...}: {
   imports = [
     ./nh.nix
-    ./tools.nix
-    ./dev # 自动寻找 modules/dev/default.nix
+    ./basic_tools.nix
+    ./tools # 自动寻找 modules/tools/default.nix
+    ./env # 自动寻找 modules/env/default.nix
     ./shell # 自动寻找 modules/shell/default.nix
   ];
 }
--- modules/tools/default.nix
+++ modules/tools/default.nix
@@ -6,11 +6,7 @@
     ./bat.nix
     ./btop.nix
     ./htop.nix
-    ./direnv.nix
-    ./python.nix
     ./yazi.nix
     ./zellij.nix
-    # ./broot.nix
-    # ./snitch.nix
   ];
 }
